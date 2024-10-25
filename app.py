6cfa6667464c6b3690b3504b09c1ecf175a7bc5e
AugustynJ <kuba.augustyn2002@gmail.com>
2024-10-25 08:59:05 +0200

    Webpage #1

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for, request
+import os
 
 app = Flask(__name__)
+template_dir = os.path.relpath('./templates')
 
 @app.route('/')
 def index():
@@ -10,9 +12,17 @@ def index():
 def home():
     return render_template('home.html')
 
-@app.route('/exercise_1')
-def exercise_1():
-    return "TBD"
+@app.route('/method_1')
+def method_1():
+    return render_template('method_1.html')
+
+@app.route('/method_1/client')
+def method_1_client():
+    return render_template('method_1/client.html')
+
+@app.route('/method_1/server')
+def method_1_server():
+    return render_template('method_1/server.html')
 
 def start_app():
     app.run(host="127.0.0.1", port=5000, debug=False)
