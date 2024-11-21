4bb8b8f974d71a1cc8984681ceb8b1f92492e59a
Jakub Augustyn <jakub.augustyn@comarch.pl>
2024-11-21 13:44:34 +0100

    Extended webpage

--- app.py
+++ app.py
@@ -56,6 +56,103 @@ def method_1_server():
 
 
 
+@app.route('/method_2')
+def method_2():
+    return render_template('method_2.html')
+
+@app.route('/method_2/client', methods=['GET', 'POST'])
+def method_2_client():
+
+    return render_template('method_2/client.html')
+
+@app.route('/method_2/server', methods=['GET', 'POST'])
+def method_2_server():
+
+    return render_template('method_2/server.html')
+
+
+
+@app.route('/method_3')
+def method_3():
+    return render_template('method_3.html')
+
+@app.route('/method_3/client', methods=['GET', 'POST'])
+def method_3_client():
+
+    return render_template('method_3/client.html')
+
+@app.route('/method_3/server', methods=['GET', 'POST'])
+def method_3_server():
+
+    return render_template('method_3/server.html')
+
+
+
+@app.route('/method_4')
+def method_4():
+    return render_template('method_4.html')
+
+@app.route('/method_4/client', methods=['GET', 'POST'])
+def method_4_client():
+
+    return render_template('method_4/client.html')
+
+@app.route('/method_4/server', methods=['GET', 'POST'])
+def method_4_server():
+
+    return render_template('method_4/server.html')
+
+
+
+@app.route('/method_5')
+def method_5():
+    return render_template('method_5.html')
+
+@app.route('/method_5/client', methods=['GET', 'POST'])
+def method_5_client():
+
+    return render_template('method_5/client.html')
+
+@app.route('/method_5/server', methods=['GET', 'POST'])
+def method_5_server():
+
+    return render_template('method_5/server.html')
+
+
+
+@app.route('/method_6')
+def method_6():
+    return render_template('method_6.html')
+
+@app.route('/method_6/client', methods=['GET', 'POST'])
+def method_6_client():
+
+    return render_template('method_6/client.html')
+
+@app.route('/method_6/server', methods=['GET', 'POST'])
+def method_6_server():
+
+    return render_template('method_6/server.html')
+
+
+
+@app.route('/method_7')
+def method_7():
+    return render_template('method_7.html')
+
+@app.route('/method_7/client', methods=['GET', 'POST'])
+def method_7_client():
+
+    return render_template('method_7/client.html')
+
+@app.route('/method_7/server', methods=['GET', 'POST'])
+def method_7_server():
+
+    return render_template('method_7/server.html')
+
+
+
+
 def start_app():
     app.run(host="0.0.0.0", port=5000, debug=True)
 
